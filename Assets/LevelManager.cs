909fc4255e9b9e259fe545d0ae3f8a59d0cd20f5
crikan <crikancoder@gmail.com>
2020-07-08 15:02:35 +0200

    full_commit_v1

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CargaEscena(string nombreEscena) {
+        SceneManager.LoadScene(nombreEscena);
+    }
+
+    public void Salir() {
+        Application.Quit();
+        Debug.Log("Se ha salido del juego");
+    }
+}
